c157d8d91f0f516c08edc0735f736d94321a6902
Taormina7575 <vinnie.taormina@gmail.com>
2021-07-09 18:25:39 -0400

    final

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "./../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -14,17 +15,7 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
-
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  const [values, showSuccessMessage, handleChanges, handleSubmit] = useForm(initialValue);
 
   return (
     <>
@@ -36,6 +27,7 @@ const CheckoutForm = (props) => {
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
+            placeholder='firstname'
           />
         </label>
         <label>
@@ -44,6 +36,7 @@ const CheckoutForm = (props) => {
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
+            placeholder='lastname'
           />
         </label>
         <label>
@@ -52,19 +45,20 @@ const CheckoutForm = (props) => {
             name="address"
             value={values.address}
             onChange={handleChanges}
+            placeholder='address'
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={values.city} onChange={handleChanges} placeholder='city'/>
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={values.state} onChange={handleChanges} placeholder='state'/>
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={values.zip} onChange={handleChanges} placeholder='zip'/>
         </label>
         <button>Checkout</button>
       </form>
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm/>)
+    const nameInput = screen.getByPlaceholderText(/firstname/i)
+    const lastNameInput = screen.getByPlaceholderText(/lastname/i)
+    const addressInput = screen.getByPlaceholderText(/address/i)
+    const cityInput = screen.getByPlaceholderText(/city/i)
+    const stateInput = screen.getByPlaceholderText(/state/i)
+    const zipInput = screen.getByPlaceholderText(/zip/i)
+    const button = screen.getByRole('button')
+
+    userEvent.type(nameInput, 'vincenzo')
+    userEvent.type(lastNameInput, 'taormina')
+    userEvent.type(addressInput, 'address')
+    userEvent.type(cityInput, 'newfield')
+    userEvent.type(stateInput, 'nj')
+    userEvent.type(zipInput, '08344')
+    userEvent.click(button)
+
+    const successMessage = screen.getByTestId('successMessage')
+    expect(successMessage).toBeInTheDocument()
+});
